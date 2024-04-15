bacc4ddceab79475d295ec08a45c3e7dc2b5cc72
Vladislave <fadeeverynight@gmail.com>
2024-04-15 09:08:10 +0300

    create a registration form

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -7,7 +7,7 @@ import { observer } from "mobx-react-lite";
 const NavBar = observer(() => {
   const { user } = useContext(Context)
   return (
-  <Navbar bg="dark" data-bs-theme="dark">
+  <Navbar className="position-absolute top-0 right-0 w-100" bg="dark" data-bs-theme="dark">
     <Container>
       <Nav.Link style={{color: "white",}} to={SHOP_ROUTE}>КупиДевайс</Nav.Link>
         { user.isAuth ?
@@ -20,7 +20,6 @@ const NavBar = observer(() => {
               <Button variant="outline-light" onClick={() => user.setIsAuth(true)}>Авторизация</Button>
             </Nav>
               }
-      
     </Container>
   </Navbar>
   )
